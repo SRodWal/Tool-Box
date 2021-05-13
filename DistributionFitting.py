60a6a115751d3a3e59ce7f775514252e31c35e71
Samuel Rod-Wal <83095093+SRodWal@users.noreply.github.com>
2021-05-13 00:18:26 -0500

    Update DistributionFitting.py

--- DistributionFitting.py
+++ DistributionFitting.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
-
-import datetime
 import pandas as pd
 import numpy as np
+import datetime as dt
+import dateutil # Nos ayuda a agregat meses al datetime
 import seaborn as sb
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
+import scipy # Ayuda a crear distribuciones y arreglos
 
-import zipfile
-import scipy
+def monthNum(num):
+    return {1 : "Jan", 2:"Feb",3:"Mar",4:"Apr",5:"May",6:"Jun",7:"Jul",8:"Aug",9:"Sep",10: "Oct",11:"Nov",12:"Dec"}[num]
 
-from playsound import playsound # Just for fun
-### Given a data set
 data = [ ["May de 2016",	1.4588,	3.6051,	3.9370,	2.3989,	2.2441], 
         ["July de 2016",	1.4588,	3.6051,	3.9370,	2.3989,	2.2441],
         ["November de 2016",	1.4911,	3.6848,	4.0315,	2.4603,	2.2961],
@@ -33,53 +30,74 @@ data = [ ["May de 2016",	1.4588,	3.6051,	3.9370,	2.3989,	2.2441],
         ["January de 2021",	3.4281,	4.4608,	4.4883,	2.7093,	2.5355],
         ["April de 2021",	3.3657,	4.3796,	4.4082,	2.6437,	2.4725],
         ]
-medpow = [246.5493,246.5493,250.7668,252.3153, 252.4918,252.4920,252.6116, 
+MTpot = [246.5493,246.5493,250.7668,252.3153, 252.4918,252.4920,252.6116, 
           253.4864, 257.6204, 260.6356, 262.8404, 287.7614, 295.9551, 297.2365, 
           297.9744, 304.7970, 305.6773, 310.1292, 311.3043, 313.4589]
-cols = ["Fecha","Residencial (< 50 kWh)", "Residencial (> 50 kWh)", "Baja Tensión", "Media Tensión", "Alta Tensión"]
+cols = ["Fecha","Residencial (< 50 kWh)", "Residencial (> 50 kWh)", "Baja Tensión", "Media Tensión","Potencia @ MT", "Alta Tensión"]
+#### Generar dataframe de tarifas
+timevec = [dt.datetime(2016,5,1)]
+for n in range(1,len(data)):
+    timevec.append(timevec[-1]+dateutil.relativedelta.relativedelta(months=3))
+datarray = np.array(data)
+datarray = np.insert(datarray, 5, MTpot, axis = 1)
+df = pd.DataFrame (datarray, columns = cols)
+df["Fecha"]=timevec
+df = df.set_index(cols[0])
 
-# Crear vector de tiempo
-yrs = [i for i in range(2016,2022)]
-timevec = []
-for y in yrs:
-    for m in range(1,13):
-        for dat in data:
-            if (str(y) in dat[0])&(monthNum(m) in dat[0]):            
-                timevec.append(datetime.datetime(y, m, 1))
-            
-df = pd.DataFrame(np.array(data), columns = cols)
-df[df.columns[0]] = pd.DataFrame(np.array(timevec))
-dftar = df.drop(df.columns[0], axis=1)
-dftar.insert(4, "Potencia MT", medpow, True)
-for name in dftar.columns:
-        df = dftar[name]
-        df = pd.to_numeric(df, errors = "coerce")
-        dftar[name] = df
-powdf = pd.DataFrame(medpow, columns = ["Potencia MT"])
-#dftar.describe().to_excel("Tarifas.xlsx")
-
-
-tar = []
-for name in dftar.columns[2:5]:
-    tarch = []  
-    for i in range(0,len(data)-1):
-        tarch.append(2*(dftar[name][i+1]-dftar[name][i])/(dftar[name][i]+dftar[name][i+1])*100)
-    tar.append(tarch)
-
-dftarch = pd.DataFrame(np.array(tar).T,columns = dftar.columns[2:5])    
-fig = plt.figure(figsize = (8,6))
-[sb.distplot(dftarch[i]) for i in dftarch.columns ]
-fig.legend(labels = dftar.columns[2:5])
+#### Analizar cambios pocentuales de tarifas
+datarray = np.delete(datarray, 0,1)
+datarray = datarray.astype(float)
+Tchlist = [(100*np.divide(np.subtract(datarray[1],datarray[0]),datarray[1])).tolist()]
+for i in range(1,len(data)-1):
+    trray = (100*np.divide(np.subtract(datarray[i+1],datarray[i]),datarray[i+1])).tolist()
+    Tchlist.append(trray)
+#Dataframe con cambios porcentuales
+dft = pd.DataFrame(Tchlist, columns = cols[1:7])
+#Graficar histograma de cambios porcentuales
+plt.figure(num = 1, figsize = (6,4))
+[sb.kdeplot(dft[name]) for name in dft.columns]
+plt.legend(dft.columns)
 plt.show()
+# Crear distribuciones
+variables = cols[1:7]
+disttype = ['alpha','anglit','arcsine','beta','betaprime','bradford','burr','burr12','cauchy','chi','chi2','cosine','dgamma','dweibull','erlang','expon','exponnorm','exponweib','exponpow','f','fatiguelife','fisk','foldcauchy','foldnorm','frechet_r','frechet_l','genlogistic','genpareto','gennorm','genexpon','genextreme','gausshyper','gamma','gengamma','genhalflogistic','gilbrat','gompertz','gumbel_r','gumbel_l','halfcauchy','halflogistic','halfnorm','halfgennorm','hypsecant','invgamma','invgauss','invweibull','johnsonsb','johnsonsu','kstwobign','laplace','levy','levy_l','logistic','loggamma','loglaplace','lognorm','lomax','maxwell','mielke','nakagami','ncx2','ncf','nct','norm','pareto','pearson3','powerlaw','powerlognorm','powernorm','rdist','reciprocal','rayleigh','rice','recipinvgauss','semicircular','t','triang','truncexpon','truncnorm','tukeylambda','uniform','wald','weibull_min','weibull_max']
+legend = disttype+["Data-Hist"]
+x = np.linspace(-40,40,100) # Rango de cambios porcentuales
 
-fits = []
-for name in dftarch.columns:
-    x = np.linspace(-20,20,100)
-    mean, var  = scipy.stats.distributions.norm.fit(dftarch[name])
-    fitted_data = scipy.stats.distributions.norm.pdf(x, mean, var)
-    fits.append(fitted_data)
-[plt.plot(x, y) for y in fits]
-plt.show    
+def probadensityfun(dtype,x,stats):
+    fit = getattr(scipy.stats,dtype).pdf
+    if len(stats)==2:
+        distfit = fit(x, stats[0], stats[1])
+    if len(stats)==3:    
+        distfit = fit(x, stats[0], stats[1], stats[2])
+    if len(stats)==4:
+        distfit = fit(x, stats[0], stats[1], stats[2], stats[3])
+    if len(stats)==5:
+        distfit = fit(x, stats[0], stats[1], stats[2], stats[3], stats[4])
+    if len(stats)==6:
+        distfit = fit(x, stats[0], stats[1], stats[2], stats[3], stats[4],stats[5])
+    return distfit 
+   
+for n, name in zip(range(0, len(variables)), variables):
+    plt.figure(num = n, figsize = (8,6))
+    sb.histplot(dft[name], stat = "density", alpha = 0.5)
+    plt.title(name)
+    
+    res = []
+    distfits = []
+    for dtype in disttype:
+        stats = getattr(scipy.stats,dtype).fit(dft[name])
+        distfits.append(probadensityfun(dtype,x,stats))
+        kstest = scipy.stats.kstest(dft[name], dtype, args = stats)
+        res.append((dtype, kstest[0],kstest[1],distfits[-1]))
+    res.sort(key=lambda x:float(x[2]), reverse=True)
+    print("*------------------ Data Type: "+name+" ------------------*")
+    
+    for j in res[0:10]:
+        print("{}: statistic={}, pvalue={}".format(j[0], j[1], j[2]))
+    [plt.plot(x,den[3]) for den in res[0:10]]
+    plt.legend([f[0] for f in res[0:10] ])
+    plt.show() 
       
       
     
